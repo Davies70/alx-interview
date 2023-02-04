f60867f3bce35b7878e9a092257dd4cf172ae93f
Ajayi Davies <ajayidavies7@gmail.com>
2023-02-04 10:01:07 +0100

    refactored code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,26 +9,18 @@ def canUnlockAll(boxes):
     ''' check if all boxes can be unlocked'''
     if len(boxes) == 0:
         return False
-    hash = {}
-    for index, box in enumerate(boxes):
-        hash[index] = box
-
     keysafe = []
-    i = 0
 
-    for box in boxes:
-        for num in box:
-            keysafe.append(num)
+    for keys in boxes[0]:
+        keysafe.append(keys)
 
-    for key in hash.keys():
-        if key != 0:
-            if key not in keysafe:
-                return False
-            if key in boxes[key]:
-                occurence = boxes[key].count(key)
-                while i < occurence:
-                    keysafe.remove(key)
-                    i += 1
-                if key not in keysafe:
-                    return False
+    for key in keysafe:
+        for index, value in enumerate(boxes):
+            if index in keysafe and index != 0:
+                keysafe = keysafe + value
+
+    for i in range(1, len(boxes)):
+        if i not in keysafe:
+            return False
+
     return True
