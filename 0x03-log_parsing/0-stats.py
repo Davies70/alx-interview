fd5276c8b4723fd34910ea1c0d2ef473a55759ba
Ajayi Davies <ajayidavies7@gmail.com>
2023-02-19 21:46:44 +0100

    check chat

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,53 +2,41 @@
 
 import sys
 
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
+# Initialize variables
 total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+status_code_counts = {}
 
 try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+    # Read from stdin line by line
+    for i, line in enumerate(sys.stdin, 1):
+        # Parse the line
+        parts = line.split()
+        if len(parts) != 7:
+            # Skip the line if the format is incorrect
+            continue
+        ip_address, _, _, path, status_code, file_size, _ = parts
+        if path != '/projects/260':
+            # Skip the line if the path is not '/projects/260'
+            continue
+        try:
+            file_size = int(file_size)
+            status_code = int(status_code)
+        except ValueError:
+            # Skip the line if the file size or status code is not an integer
+            continue
 
-        if len(parsed_line) > 2:
-            counter += 1
+        # Update the metrics
+        total_file_size += file_size
+        status_code_counts[status_code] = status_code_counts.get(status_code, 0) + 1
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+        # Print the metrics every 10 lines
+        if i % 10 == 0:
+            print(f"Total file size: {total_file_size}")
+            for status_code in sorted(status_code_counts.keys()):
+                print(f"{status_code}: {status_code_counts[status_code]}")
+            print()
+except KeyboardInterrupt:
+    # Print the final metrics on keyboard interruption
+    print(f"\nTotal file size: {total_file_size}")
+    for status_code in sorted(status_code_counts.keys()):
+        print(f"{status_code}: {status_code_counts[status_code]}")
