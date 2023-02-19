c6fc8e2f568ee4ebe04d494a78bd99f49a5aa24f
Ajayi Davies <ajayidavies7@gmail.com>
2023-02-19 21:38:54 +0100

    try again

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,46 @@
 #!/usr/bin/python3
-
-import sys
-
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+'''reads stdin line by line and computes metrics'''
+from sys import stdin
+import re
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
-
+comp_str_1 = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
+comp_str_2 = r'\[\d{4}-\d\d-\d\d \d\d:\d\d:\d\d.\d{6}\] '
+comp_str_3 = r'"GET /projects/260 HTTP/1.1" (\d{3}) (\d{1,4})'
+total_size = 0
+code_no = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+    line_count = 0
+    for line in stdin:  # Reads through input stream line by line
+        compare_string = re.compile(comp_str_1 + comp_str_2 + comp_str_3)
+        reg_search = compare_string.search(line)
+        if reg_search:
+            file_size_reg_match = re.compile(comp_str_3)
+            rty = file_size_reg_match.search(line)
+            status_code, file_size = rty.groups()  # type: ignore
+            if line_count == 10:
+                line_count = 0
+                print('File size: {}'.format(total_size))
+                for i in code_no:
+                    if code_no[i]:
+                        print('{}: {}'.format(i, code_no[i]))
+            line_count += 1
+            try:
+                file_size = int(file_size)
+                total_size += file_size
 
-        if len(parsed_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+                status_code = int(status_code)
+                if status_code in code_no:
+                    code_no[status_code] += 1
+            except ValueError:
+                pass
+    print('File size: {}'.format(total_size))
+    for i in code_no:
+        if code_no[i]:
+            print('{}: {}'.format(i, code_no[i]))
+except KeyboardInterrupt:
+    print('File size: {}'.format(total_size))
+    for i in code_no:
+        if code_no[i]:
+            print('{}: {}'.format(i, code_no[i]))
+    raise
