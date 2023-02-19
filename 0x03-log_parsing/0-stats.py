91748f8a56dd7847e7244f781dba9bd676e09c0e
Ajayi Davies <ajayidavies7@gmail.com>
2023-02-19 21:40:49 +0100

    second test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
-'''reads stdin line by line and computes metrics'''
-from sys import stdin
-import re
+"""
+Task - Script that reads stdin line by line and computes metrics
+"""
+
+import sys
 
 
-comp_str_1 = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
-comp_str_2 = r'\[\d{4}-\d\d-\d\d \d\d:\d\d:\d\d.\d{6}\] '
-comp_str_3 = r'"GET /projects/260 HTTP/1.1" (\d{3}) (\d{1,4})'
-total_size = 0
-code_no = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-try:
-    line_count = 0
-    for line in stdin:  # Reads through input stream line by line
-        compare_string = re.compile(comp_str_1 + comp_str_2 + comp_str_3)
-        reg_search = compare_string.search(line)
-        if reg_search:
-            file_size_reg_match = re.compile(comp_str_3)
-            rty = file_size_reg_match.search(line)
-            status_code, file_size = rty.groups()  # type: ignore
-            if line_count == 10:
-                line_count = 0
-                print('File size: {}'.format(total_size))
-                for i in code_no:
-                    if code_no[i]:
-                        print('{}: {}'.format(i, code_no[i]))
-            line_count += 1
-            try:
-                file_size = int(file_size)
-                total_size += file_size
+if __name__ == "__main__":
+    st_code = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
+    count = 1
+    file_size = 0
 
-                status_code = int(status_code)
-                if status_code in code_no:
-                    code_no[status_code] += 1
-            except ValueError:
-                pass
-    print('File size: {}'.format(total_size))
-    for i in code_no:
-        if code_no[i]:
-            print('{}: {}'.format(i, code_no[i]))
-except KeyboardInterrupt:
-    print('File size: {}'.format(total_size))
-    for i in code_no:
-        if code_no[i]:
-            print('{}: {}'.format(i, code_no[i]))
-    raise
+    def parse_line(line):
+        """ Read, parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        """print stats in ascending order"""
+        print("File size: {}".format(file_size))
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print("{}: {}".format(key, st_code[key]))
+
+    try:
+        for line in sys.stdin:
+            file_size += parse_line(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
