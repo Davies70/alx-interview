7131f07f2c3311d6477e8399cc8790755f79449b
Ajayi Davies <ajayidavies7@gmail.com>
2023-04-07 02:04:20 +0200

    pycode

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -47,7 +47,7 @@ def is_prime(n):
     Returns True if the input number n is prime, False otherwise.
     """
     if n < 2:
-        return False # 0 and 1 are not primes
+        return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
